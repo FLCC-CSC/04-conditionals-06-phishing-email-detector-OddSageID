32afc714b72dbf377e5aa00a58d24b8a6673638b
Kevin Hormaza <khormaza@fingerlakes.edu>
2025-10-06 08:11:25 +0000

    phishing_email

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -1,8 +1,9 @@
 # FILE NAME - phishing_email_detector.py
-
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: kevin hormaza 
+# DATE: October 6, 2025
+# BRIEF DESCRIPTION: This program analyzes an email subject line for possible phishing indicators
+# based on common red flag keywords. It outputs a security assessment rating
+# depending on the level of risk detected.
 
 
 
@@ -18,7 +19,23 @@
 
 
 
+subject = input("Enter the email subject line: ")
 
+lower_subject = subject.lower()
+
+print("\nSECURITY ASSESSMENT:")
+
+if "urgent" in lower_subject or "immediate action required" in lower_subject:
+    print("HIGH RISK: Possible phishing attempt.")
+elif "win" in lower_subject or "free" in lower_subject:
+    print("MEDIUM RISK: Suspicious offer detected.")
+elif "password reset" in lower_subject:
+    print("LOW RISK: Verify legitimacy with sender.")
+else:
+    print("No phishing indicators detected.")
+
+print("------------------------")
+print(f'Analyzed subject: "{subject}"')
 
 
 
@@ -77,7 +94,8 @@ Analyzed subject: "Did you request a password reset?"
 '''
 
 1. Was using `in` difficult or was it natural?
-
+Once i understood that 'in' checks substrings inside text.
+converting the input to lowercase made the matching simple and consistent.
 
 
 
@@ -98,7 +116,7 @@ Please gauge your utilization of AI on the following spectrum. Place an "X" in f
 of the appropriate response. Only choose one of the following:
 
 [ ] I did not use AI at all for this lab.
-[ ] I wrote the initial draft of the software but had AI help me make it better.
+[X] I wrote the initial draft of the software but had AI help me make it better.
 [ ] I fed the lab description to AI and had it generate a response but I modified it.
 [ ] AI created the entire program for me.
 
@@ -110,7 +128,7 @@ like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Go
 may impede your understanding. Please rate how well you understand the concepts in this lab: 
 
 [ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+[X] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
 [ ] I'm solid. Totally got it.
 
